1f26523760f965cf5579e98bc2405bd69ed41393
darwin <202280141@psu.palawan.edu.ph>
2024-12-10 12:16:40 +0800

    START API_CALLING_DBASE

---
+++ API.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from http import HTTPStatus
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'minimized_inventory_control_for_sports_centers'
+
+mysql = MySQL(app)
